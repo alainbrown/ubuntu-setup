cd9c05ca6731ee600e409d3343aeacf33726b593
Alain Brown <alain.brown@gmail.com>
2014-05-23 22:02:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install gnome-terminal file-roller gedit vlc ubuntu-restricted-extras ttf-ubuntu-font-family software-center synaptic git mercurial oracle-java7-installer
