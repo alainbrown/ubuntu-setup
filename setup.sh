73c9617e6a0dcdb59d1323047f500673a94c4d30
Alain Brown <alain.brown@gmail.com>
2014-05-23 22:15:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!bin/bash
+
+#setup go
+sudo apt-get install curl
+
+curl https://storage.googleapis.com/golang/go1.2.2.linux-amd64.tar.gz
+tar -C /usr/local -xvf go1.2.2.linux-amd64.tar.gz
+mkdir gopath
+echo "export GOPATH=$HOME/gopath" >> ~/.bash_rc
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> ~/.bash_rc
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install gnome-terminal file-roller gedit vlc ubuntu-restricted-extras ttf-ubuntu-font-family software-center synaptic git mercurial oracle-java7-installer
